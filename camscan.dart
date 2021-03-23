736e9f7296afc0d435b93c5c51ee59276352f413
Pritish Mair <59889857+Pmair20@users.noreply.github.com>
2021-03-23 20:27:11 +0530

    Add files via upload
    Minor changes in subclasss and new file for cam scanning

---
+++ camscan.dart
@@ -0,0 +1,165 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../main.dart';
+
+class Camscan extends StatefulWidget {
+  final List id ;
+  Camscan(this.id);
+
+  @override
+  _CamscanState createState() => _CamscanState();
+}
+
+class _CamscanState extends State<Camscan> {
+  CameraController _controller;
+  File imageFile;
+  List present_id;
+
+
+  Future scan(String imgpath) async{
+    final File imageFile = File(imgpath);
+
+    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(imageFile);
+
+    final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
+
+    final VisionText visionText = await textRecognizer.processImage(visionImage);
+
+    //String pattern = r"^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)$";
+    //String pattern2 = r".:";
+    //RegExp regEx = RegExp(pattern);
+
+    String id = "";
+
+    for (TextBlock block in visionText.blocks) {
+      for (TextLine line in block.lines) {
+        // Checking if the line contains an email address
+        if (line.text.toString().contains("ID No.:")) {
+          List idno = line.text.toString().split(" ");
+          if (widget.id.contains(idno[idno.length-1])){
+            id += line.text;
+            present_id.add(idno[idno.length - 1]);
+            break;
+          }
+          else{
+            print("Student does not exist");
+            break;
+          }
+        }
+      }
+    }
+    print(id);
+  }
+
+  Future<String> _takePicture() async {
+
+    // Checking whether the controller is initialized
+    if (!_controller.value.isInitialized) {
+      print("Controller is not initialized");
+      return null;
+    }
+
+    // Formatting Date and Time
+    String dateTime = DateFormat.yMMMd()
+        .addPattern('-')
+        .add_Hms()
+        .format(DateTime.now())
+        .toString();
+
+    String formattedDateTime = dateTime.replaceAll(' ', '');
+    print("Formatted: $formattedDateTime");
+
+    // Retrieving the path for saving an image
+    final Directory appDocDir = await getApplicationDocumentsDirectory();
+    final String visionDir = '${appDocDir.path}/Photos/Vision\ Images';
+    await Directory(visionDir).create(recursive: true);
+    final String imagePath = '$visionDir/image_$formattedDateTime.jpg';
+
+    // Checking whether the picture is being taken
+    // to prevent execution of the function again
+    // if previous execution has not ended
+    if (_controller.value.isTakingPicture) {
+      print("Processing is in progress...");
+      return null;
+    }
+
+    try {
+      // Captures the image and saves it to the
+      // provided path
+      await _controller.takePicture(imagePath);
+    } on CameraException catch (e) {
+      print("Camera Exception: $e");
+      return null;
+    }
+
+    return imagePath;
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    present_id = [];
+
+    _controller = CameraController(cameras[0], ResolutionPreset.medium);
+    _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CamScan'),
+      ),
+      body: _controller.value.isInitialized
+          ? Stack(
+        children: <Widget>[
+          CameraPreview(_controller),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: RaisedButton.icon(
+                icon: Icon(Icons.camera),
+                label: Text("Click"),
+                onPressed: () async {
+                  await _takePicture().then((String path) async  {
+                    if (path != null) {
+                      await scan(path) ;
+                    }
+                  });
+                },
+              ),
+            ),
+          )
+        ],
+      )
+          : Container(
+        color: Colors.black,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+
+  }
+
+}
