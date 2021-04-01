50b2537e38b44ade52d0886cafcb8e3e82a07628
interstelllar <62997585+interstelllar@users.noreply.github.com>
2021-04-01 18:17:52 +0530

    Add files via upload

--- excelsheet.dart
+++ excelsheet.dart
@@ -37,7 +37,8 @@ class time_subject_excel extends State<Excelsheet>{
         lastDate: DateTime(2101));
     if (picked != null && picked != selectedDate)
       setState(() {
-        print(picked);
+        String a = picked.day.toString() + "-" + picked.month.toString() + "-" + picked.year.toString();
+        print(a);
         selectedDate = picked;
       });
 
@@ -69,26 +70,50 @@ class time_subject_excel extends State<Excelsheet>{
       ),
       body:
         Padding(
-          padding: const EdgeInsets.all(12.0),
+          padding: const EdgeInsets.all(14.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("Select subject"),
-              DropdownButton<String>(
-                items: _subjects.map((String dropDownStringItem){
-                  return DropdownMenuItem<String>(
-                    value: dropDownStringItem,
-                    child: Text(dropDownStringItem),
-                  );
-                }).toList(),
-                onChanged: (String newValueSelected) {
-                  setState(() {
-                    this.subjectIsSelected = newValueSelected;
-                  });
-                },
-                value: subjectIsSelected,
+              Padding(padding: const EdgeInsets.only(bottom: 25.0),
+                child:Text(
+                "Select Subject",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w900,
+                ),
               ),
-              SizedBox(height: 10.0,),
+              ),
+              InputDecorator(decoration: InputDecoration(
+                labelText: 'Subject',
+                border: const OutlineInputBorder(),
+
+              ),
+                child:DropdownButtonHideUnderline(
+                  child:DropdownButton(
+
+                    elevation: 8,
+                    icon: Icon(Icons.arrow_drop_down),
+                    items: _subjects.map((String dropDownStringItem){
+                      return DropdownMenuItem<String>(
+                        value: dropDownStringItem,
+                        child: Text(
+                            dropDownStringItem,
+                          style: TextStyle(
+                            fontSize: 20.0
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String newValueSelected) {
+                      setState(() {
+                        this.subjectIsSelected = newValueSelected;
+                      });
+                    },
+                    value: subjectIsSelected,
+                  ),
+                ),
+              ),
+              SizedBox(height: 25.0,),
               Card(
                   elevation: 4,
                   child: ListTile(
@@ -117,7 +142,7 @@ class time_subject_excel extends State<Excelsheet>{
                       onPressed: () {},
                     ),
                   )),
-              SizedBox(height: 10.0,),
+              SizedBox(height: 12.0,),
               Card(
                   elevation: 4,
                   child: ListTile(
@@ -146,10 +171,72 @@ class time_subject_excel extends State<Excelsheet>{
                       onPressed: () {},
                     ),
                   )),
-              SizedBox(height: 10.0,),
-              ElevatedButton(onPressed: () {},
-                child: Text("Generate excel sheet"),
+              SizedBox(height: 50.0,),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(0.0),
+                      primary: Colors.white,
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(75.0)),
+                      textStyle: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                        fontSize: 18,
+                        fontFamily: "Bold",
+                      ),
+                    ),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(colors: <Color>[Color(0xFF3a5af9), Color(0xFF7449fa)]),
+                        borderRadius: BorderRadius.all(Radius.circular(80.0)),
+                      ),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+                          child: Text(
+                            "Generate Excel sheet",
+                            style: TextStyle(fontSize: 18, fontFamily: "Bold"),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    )
+                ),
               ),
+              // ElevatedButton.icon(
+              //    icon: Icon(Icons.view_list_rounded),
+              //   onPressed: () {},
+              //   label:
+              //   Text(
+              //       "Generate excel sheet",
+              //     style: TextStyle(
+              //       fontSize: 15.0,
+              //     ),
+              //   ),
+              //   style: ElevatedButton.styleFrom(
+              //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+              //   ),
+                // child: Container(
+                //   width: MediaQuery.of(context).size.width,
+                //   decoration: const BoxDecoration(
+                //     gradient: LinearGradient(colors: <Color>[Color(0xFF3a5af9), Color(0xFF7449fa)]),
+                //     borderRadius: BorderRadius.all(Radius.circular(80.0)),
+                //   ),
+                //   child: Center(
+                //     child: Padding(
+                //       padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+                //       child: Text(
+                //         "Sign In",
+                //         style: TextStyle(fontSize: 18, fontFamily: "Bold"),
+                //         textAlign: TextAlign.center,
+                //       ),
+                //     ),
+                //   ),
+                // )
+              // ),
             ],
           ),
         ),
