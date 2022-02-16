94f8c0c7b09e1893b2862131e469c80345e6f6b9
sharvin18 <dedhiasharvin@gmail.com>
2022-02-16 11:48:48 +0530

    changes in db

--- jstore_automation/uploader.js
+++ jstore_automation/uploader.js
@@ -11,6 +11,8 @@ const firestore = admin.firestore();
 const path = require("path");
 const fs = require("fs");
 const directoryPath = path.join(__dirname, "files");
+console.log("Start");
+let i=0;
 
 fs.readdir(directoryPath, function(err, files) {
   if (err) {
@@ -19,23 +21,40 @@ fs.readdir(directoryPath, function(err, files) {
 
   files.forEach(function(file) {
     var lastDotIndex = file.lastIndexOf(".");
+    if(file == "comps_data.json"){      
+      
+      var menu = require("./files/" + file);
+      console.log(file)
+      menu.forEach(function(obj) {
+        
+        data = {
+          "name": obj.name,
+          "id": obj.id,
+          "email": obj.email,
+          "phone": obj.phone,
+          "elective": obj.elective,
+          "roll": obj.roll
+        };
+        console.log(data.name);
+        
+        firestore
+          .collection(obj.branch)
+          .doc(obj.year)
+          .collection("students")
+          .doc(obj.id)
+          .set(data)
+          .then(function(docRef) {
+            console.log("Document written");
+            i++;
+          })
+          .catch(function(error) {
+            console.error("Error adding document: ", error);
+          });
 
-    var menu = require("./files/" + file);
-    console.log(menu)
-    menu.forEach(function(obj) {
-      firestore
-        .collection("eyantra")
-        .doc("EY")
-        .collection("EYANTRA")
-        .doc(obj.id)
-        .set(obj)
-        .then(function(docRef) {
-          console.log("Document written");
-        })
-        .catch(function(error) {
-          console.error("Error adding document: ", error);
-        });
-    });
+      });
+      console.log(`uploader executed successfully ${i}`);
+    }
   });
 });
+
 
